43bb4c8046cdbbc6bbc9a2deb97e2e56150197a4
burhan <burhanweb111@gmail.com>
2025-06-01 12:03:45 +0530

    Fix package.json conflict

--- client/utils/axios.js
+++ client/utils/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:8000/api/v1",
-=======
-  baseURL: "http://localhost:3000/api/v1",
->>>>>>> 99436b0bbaf9e5582fbed4fc3e6505a91c0bb242
+  baseURL: "https://ai-resume-3-nnuj.onrender.com/api/v1",
   withCredentials: true,
 });
 
